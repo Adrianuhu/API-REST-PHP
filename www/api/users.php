ae08013033a2d81958f61bf89ea0d677ec0dfa8b
J. Duron <Jduroncortiz@gmail.com>
2024-07-11 05:14:47 +0200

    Updates

--- www/api/users.php
+++ www/api/users.php
@@ -8,19 +8,39 @@ header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
 header('Content-Type: application/json');
 header('Access-Control-Allow-Credentials: true');
 
-/** Verifica si existe una pokemonId via petición GET */
-if (isset($_GET['pokemonId'])) {
-    /// Asigna el valor de la pokemonId de la petición
-    $pokemonId = $_GET['pokemonId'];
+/** Verifica si existe una inst via petición GET */
+if (isset($_GET['inst'])) {
+    /// Asigna el valor de la inst de la petición
+    $inst = $_GET['inst'];
     $response = '';
 
     /** Se lee el metodo solicitado [GET, POST, PUT, DELETE] */
     switch ($_SERVER['REQUEST_METHOD']) {
         case 'GET': {
-            /// Obtiene el valor de id en la pokemonId api.example.com/METODO/ID
-            $id = intval(preg_replace('/[^0-9]+/', '', $pokemonId), 10);
+            echo(' GET: ');
+            echo($inst);
+
+            switch ($inst){
+
+                /** INSTRUCTION 1: Check password */
+                case 'checkPassword': {
+                    echo( ' checking password...');
+                    break;
+                }
+
+
+
+
+                default: {
+                    break;
+                }
+
+            }
+
+            /// Obtiene el valor de id en la inst api.example.com/METODO/ID
+            $id = intval(preg_replace('/[^0-9]+/', '', $inst), 10);
             
-            $response = $pokemonId;
+            $response = $inst;
 
             /// Obtiene el valor del metodo al que se dirige la petición
             // CREAR PARA QUE TE DEN TODO
@@ -39,9 +59,12 @@ if (isset($_GET['pokemonId'])) {
         case 'POST': {
             /// Obtiene el valor de DATA, en donde estan los campos y valores a usar
             $data = $_POST['data'];
-            //$data = json_decode(file_get_contents("php://input"), true);
+            $data = json_decode(file_get_contents("php://input"), true);
             /// Convierte lo obtenido en DATA a un JSON y verifica que no tenga errores, si tiene errores responde 400
-            switch ($pokemonId) {
+            echo(' POST: ');
+            echo($data);
+
+            switch ($inst) {
             case 'categoria': {
                 /// Solicita el controlador correspondiente y el metodo
                 require_once 'controllers/bas_categoria_controller.php';
@@ -59,7 +82,7 @@ if (isset($_GET['pokemonId'])) {
         }
 
         case 'DELETE': {
-            switch ($pokemonId) {
+            switch ($inst) {
             case "categoria/$id": {
                 /// Solicita el controlador correspondiente y el metodo
                 require_once 'controllers/bas_categoria_controller.php';
@@ -81,15 +104,17 @@ if (isset($_GET['pokemonId'])) {
         }
     }
 } else {
-     /// Solicita el controlador correspondiente y el metodo
-     require_once BASE_PATH . '/api/src/pokemon/controller.php';
-     $controller = new pokemonController();
-     $response = $controller->readAll();
+     /// No hace nada
 
-     /// Responde con un codigo 200 en caso de que la peticion sea correcta
-     
      setHttpResponseCode($response['status']);
+
 }
+
+/*if($response == null){
+    http_response_code(404);
+}else{
+    echo json_encode($response);
+}*/
 
 echo json_encode($response);
 
