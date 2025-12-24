5702ad7d8234f5b0f5d3775e470db0ba06a95240
Adrian Rodriguez <adrianuhu@gmail.com>
2025-12-24 17:38:05 +0100

    a

--- www/login.php
+++ www/login.php
@@ -1,75 +1,70 @@
 <!DOCTYPE html>
-<html lang="es">
+<html lang="en">
 
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
 </head>
 
 <body>
-    <div class="login-container">
-        <h1>LOGIN</h1>
+    <h1>LOGIN</h1>
 
-        <form action="login.php" method="GET">
-            <div class="form-group">
-                <label for="username">Usuario</label>
-                <input type="text" id="username" name="username" placeholder="Usuario" required>
-            </div>
+    <form action="login.php" method="GET">
+        <div>
+            <label for="username">Usuario</label>
+            <input type="text" id="username" name="username" placeholder="Usuario" required>
+        </div>
 
-            <div class="form-group">
-                <label for="password">Contraseña</label>
-                <input type="text" id="password" name="password" placeholder="Contraseña" required>
-            </div>
+        <div>
+            <label for="password">Contraseña</label>
+            <input type="text" id="password" name="password" placeholder="Contraseña" required>
+        </div>
 
-            <button type="submit">Entrar</button>
-        </form>
+        <button type="submit">Entrar</button>
+    </form>
 
-        <?php
-        if ($_SERVER["REQUEST_METHOD"] == "GET") {
-            try {
-                $manager = new MongoDB\Driver\Manager("mongodb://app:app_password@mongo:27017/appdb");
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "GET") {
+        $manager = new MongoDB\Driver\Manager("mongodb://app:app_password@mongo:27017/appdb");
 
-                $username = $_GET['username'] ?? '';
-                $password = $_GET['password'] ?? '';
+        $username = $_GET['username'];
+        $password = $_GET['password'];
 
-                // VULNERABILIDAD NOSQL INYECTION
-                // ESCRIBIENDO {"$ne": null} en el campo de contraseña se accede a cualquier usuario
-                $decoded = json_decode($password, true);
-                if (json_last_error() === JSON_ERROR_NONE && is_array($decoded)) {
-                    $password = $decoded;
-                }
+        // VULNERABILIDAD NOSQL INYECTION
+        // ESCRIBIENDO {"$ne": null} en el campo de contraseña se accede a cualquier usuario
 
-                // Filtro directo
-                $filter = [
-                    'username' => $username,
-                    'password' => $password
-                ];
 
-                // Debug: Mostrar el filtro para ver la inyección
-                echo "<br>Filter: " . json_encode($filter) . "<br>";
-
-                $query = new MongoDB\Driver\Query($filter);
-                $cursor = $manager->executeQuery('appdb.users', $query);
-                $result = $cursor->toArray();
-
-                if (!empty($result)) {
-                    $user = $result[0];
-                    ?>
-                    <div>
-                        <H1>BIENVENIDO</H1>
-                        <p><b>Usuario:</b> <?php echo $user->username; ?></p>
-                        <button onclick="window.location.href='login.php'">Salir</button>
-                    </div>
-                    <?php
-                } else {
-                    echo '<div class="error-message">Usuario o contraseña incorrectos.</div>';
-                }
-            } catch (Throwable $e) {
-                echo "Connection Error: " . $e->getMessage();
-            }
+        // Forzar que se pueda hacer una inyección NoSQL
+        $decoded = json_decode($password, true);
+        if (json_last_error() === JSON_ERROR_NONE && is_array($decoded)) {
+            $password = $decoded;
         }
-        ?>
-    </div>
+
+        // Filtro utulizado para MongoDB
+        $filter = [
+            'username' => $username,
+            'password' => $password
+        ];
+
+        $query = new MongoDB\Driver\Query($filter);
+        $cursor = $manager->executeQuery('appdb.users', $query);
+        $result = $cursor->toArray();
+
+        if (!empty($result)) {
+            $user = $result[0];
+            ?>
+            <div>
+                <H1>BIENVENIDO</H1>
+                <p>Usuario: <?php echo $user->username; ?></p>
+                <button onclick="window.location.href='login.php'">Salir</button>
+            </div>
+    <?php
+        } else {
+            echo 'Usuario o contraseña incorrectos.';
+        }
+    }
+    ?>
 </body>
 
 </html>
--- www/news.php
+++ www/news.php
@@ -8,69 +8,66 @@
 </head>
 
 <body>
-    <div class="container">
 
-        <form action="news.php" method="GET" class="search-form">
-            <input name="id" placeholder="ID..." value="<?php echo isset($_GET['id']) ? $_GET['id'] : ''; ?>" required
-                min="1">
-            <button type="submit">Buscar</button>
-        </form>
+    <form action="news.php" method="GET">
+        <input name="id" placeholder="ID..." value="<?php echo isset($_GET['id']) ? $_GET['id'] : ''; ?>" required min="1">
+        <button type="submit">Buscar</button>
+    </form>
 
-        <?php
-        $conexion = mysqli_connect("db", "user", "pass", "DB");
-        mysqli_report(MYSQLI_REPORT_OFF);
+    <?php
+    $conexion = mysqli_connect("db", "user", "pass", "DB");
+    mysqli_report(MYSQLI_REPORT_OFF);
 
 
-        if (isset($_GET['id'])) {
-            $id_raw = $_GET['id'] ?? '0';
-            $id_safe = (int) $id_raw;
+    if (isset($_GET['id'])) {
+        $id_raw = $_GET['id'] ?? '0';
+        $id_safe = (int) $id_raw;
 
-            // Consulta SQL vulnerable a inyección SQL (Blind SQL Injection)
-            // ESCRIBIENDO 3 AND 1=1 OK  2 AND 1=2 ERROR
-            // 3 AND SUBSTRING(database(),1,1)='d' OK porque nuestra DDBB se llama "DB"
-            // 3 AND SUBSTRING(database(),1,1)='a' ERROR porque nuestra DDBB se llama "DB"
-        
-            $sql = "SELECT Id, Title, Body, Datetime
-                        FROM News
-                        WHERE Id = $id_safe
-                        AND (
-                            SELECT 1
-                            FROM News
-                            WHERE Id = $id_raw
-                        )
-                        LIMIT 1
-                        ";
+        // Consulta SQL vulnerable a inyección SQL (Blind SQL Injection)
+        // ESCRIBIENDO 3 AND 1=1 OK  2 AND 1=2 ERROR
+        // 3 AND SUBSTRING(database(),1,1)='d' OK porque nuestra DDBB se llama "DB"
+        // 3 AND SUBSTRING(database(),1,1)='a' ERROR porque nuestra DDBB se llama "DB"
+    
+        $sql = "SELECT Id, Title, Body, Datetime
+                FROM News
+                WHERE Id = $id_safe
+                AND (
+                    SELECT 1
+                    FROM News
+                    WHERE Id = $id_raw
+                )
+                LIMIT 1
+                ";
 
-            // Debug query
-            echo "<br>" . $sql . "</br>";
-            $resultado = mysqli_query($conexion, $sql);
+        // Debug query
+        echo "<br>" . $sql . "</br>";
+        $resultado = mysqli_query($conexion, $sql);
 
-            if ($resultado != null && mysqli_num_rows($resultado) > 0) {
-                $news = mysqli_fetch_assoc($resultado);
-            } else {
-                $news = null;
-            }
-
-            if ($news) {
-                ?>
-
-                <div>
-                    <H1>NOTICIAS</H1>
-                    <p><b>ID:</b> <?php echo $news['Id']; ?></p>
-                    <p><b>Título:</b> <?php echo $news['Title']; ?></p>
-                    <p><b>Body:</b> <?php echo $news['Body']; ?></p>
-                    <p><b>Datetime:</b> <?php echo $news['Datetime']; ?></p>
-                </div>
-
-                <?php
-            } else {
-                echo '<div class="error-message">News no encontrado con ID: ' . $id . '</div>';
-            }
+        if ($resultado != null && mysqli_num_rows($resultado) > 0) {
+            $news = mysqli_fetch_assoc($resultado);
         } else {
-            echo '<div>Introduce un ID para ver los datos de la News</div>';
+            $news = null;
         }
-        ?>
-    </div>
+
+        if ($news) {
+            ?>
+
+            <div>
+                <H1>NOTICIAS</H1>
+                <p><b>ID:</b> <?php echo $news['Id']; ?></p>
+                <p><b>Título:</b> <?php echo $news['Title']; ?></p>
+                <p><b>Body:</b> <?php echo $news['Body']; ?></p>
+                <p><b>Datetime:</b> <?php echo $news['Datetime']; ?></p>
+            </div>
+
+            <?php
+        } else {
+            echo '<div>Noticia no encontrada con ID: ' . $id . '</div>';
+        }
+    } else {
+        echo '<div>Introduce un ID para ver los datos de la noticia</div>';
+    }
+    ?>
 </body>
 
 </html>
